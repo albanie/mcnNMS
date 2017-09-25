04355bfad246f557806ab370b73f5c1333b85c71
albanie <albanie@robots.ox.ac.uk>
2017-09-25 11:16:07 +0100

    fix paths

--- setup_mcnNMS.m
+++ setup_mcnNMS.m
@@ -2,4 +2,4 @@ function setup_mcnNMS()
 %SETUP_MCNNMS Sets up mcnNMS, by adding its folders to the Matlab path
 
   root = fileparts(mfilename('fullpath')) ;
-  addpath(root, [root '/matlab'], [root '/matlab/mex']) ;
+  addpath([root '/matlab/mex'], [root '/matlab'], root) ; % ordering here matters
